f9c70d3d1e9a0753603b06121ec201810c6dcd49
aramane <abdelramane@gmail.com>
2018-04-05 15:40:03 +0200

    modif/ListeCarType

--- view/v_ListeCarType.php
+++ view/v_ListeCarType.php
@@ -4,11 +4,12 @@
         <div class="card-body" style="text-align: center;">
             <h5 class="card-title"><?php echo $car->brand().' '.$car->model(); ?></h5>
             <div class="row container">
-                <div class="col-6 " style="text-align: center;">
-            <p><div style="height: 40px;width: 40px; background-color:<?php echo $car->color();?>; border-radius: 20px "></div></p>
+                <div class="col-5 " style="text-align: center;">
+            <p><div style="height: 40px;width: 40px; background-color:<?php echo $car->color();?>; border-radius: 20px;margin-left: 80px; "></div></p>
                 </div>
-                <div class="col-6 ">
-            <p><div style="height: 40px;width: 40px;"><?php echo $car->price();?></div></p>
+
+                <div class="col-7 ">
+            <p><div style="font-size: 25px; color: #815817;"><b><?php echo $car->price().' € par jour';?></b></div></p>
                 </div>
             </div>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
