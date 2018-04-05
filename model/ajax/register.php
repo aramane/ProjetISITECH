5698c57831233bb2a4a667f730dd04553cecf837
CHARLAT Jerome <jerome.charlat@orange.fr>
2018-04-05 12:34:04 +0200

    register AJAX backend

--- model/ajax/register.php
+++ model/ajax/register.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include("../loadClass.php");
+
+$usermanager = new UserManager();
+
+$user = new User(array(
+    'address' => $_POST['Saddress'],
+    'email' => $_POST['Semail'],
+    'name' => $_POST['Sname'],
+    'firstName' => $_POST['SfirstName'],
+    'type' => 'C',
+    'phone' => $_POST['Sphone'],
+    'password' => $_POST['Spassword']
+));
+
+if($usermanager->userExists($user))
+{
+    $response = 0;
+}
+else
+{
+    $usermanager->add($user);
+    $response = 1;
+}
+
+echo json_encode($response);
+        
+
 
