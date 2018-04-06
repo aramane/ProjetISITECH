dacc2dad5696293d60d3ef84ff8862fa1031f31d
Khelil <kbezzouh@gmail.com>
2018-04-06 11:29:24 +0200

    update bug

--- view/v_UserTableUpdate.php
+++ view/v_UserTableUpdate.php
@@ -21,7 +21,6 @@ $user = $usermanager->get($_GET['id']);
     </head>
 
     <body>
-<<<<<<< HEAD
 
 
 
@@ -30,56 +29,30 @@ $user = $usermanager->get($_GET['id']);
 
 
         </br>
-            <form action="v_UserTableUpdateConfirm.php" method="post">
-                <div class="form-group">
+            <form action="v_UserTableUpdateConfirm.php?id=<?= $_GET['id']; ?>" method="post">
+                    <div class="form-group">
                     <label for="exampleInputEmail1">Nom</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
+                    <input type="text" class="form-control" name="name"  aria-describedby="emailHelp" placeholder="Enter Name">
                 </div>
                 <div class="form-group">
                     <label for="inputName">Name</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Firstname">
+                    <input type="text" class="form-control" name="firstName" aria-describedby="emailHelp" placeholder="Enter Firstname">
                 </div>
                 <div class="form-group">
                     <label for="inputName">Address</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Address">
+                    <input type="text" class="form-control" name="address" aria-describedby="emailHelp" placeholder="Enter Address">
                 </div>
                 <div class="form-group">
                     <label for="inputName">Phone</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Phone ">
+                    <input type="text" class="form-control" name="phone" aria-describedby="emailHelp" placeholder="Enter Phone ">
                 </div>
                 <div class="form-group">
                     <label for="inputName">Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
-=======
-            <form action="v_UserTableUpdateConfirm.php?id=<?= $_GET['id']; ?>" method="post">
-                <div class="form-group">
-                    <label for="inputName">Nom</label>
-                    <input type="text" class="form-control" name="name" aria-describedby="emailHelp" value="<?php echo $user->name(); ?>" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="inputName">Prénom</label>
-                    <input type="text" class="form-control" name="firstName" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="inputName">Adresse</label>
-                    <input type="text" class="form-control" name="address" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="inputName">Téléphone</label>
-                    <input type="text" class="form-control" name="phone" aria-describedby="emailHelp" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label for="inputName">Email</label>
-                    <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter Email">
                 </div>
                 <div class="form-group">
                     <label for="inputName">Type</label>
-                    <input type="email" class="form-control" name="type" aria-describedby="emailHelp" placeholder="Enter email">
->>>>>>> 4df5284ee2fb9e8b2295b15069fd462e48c3af83
-                </div>
-                <div class="form-group">
-                    <label for="inputName">Type</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Type">
+                    <input type="email" class="form-control" name="type" aria-describedby="emailHelp" placeholder="Enter Type">
                 </div>
 
                 <div class="form-group">
@@ -92,8 +65,7 @@ $user = $usermanager->get($_GET['id']);
                     <label class="form-check-label" for="exampleCheck1">Premium</label>
                 </div>
             </br>
-
-                <button type="button" class="btn btn-secondary">Submit</button>
+             <input type="submit" class="btn" value="OK">
             </form>
         </section>
 
