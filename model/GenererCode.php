4f2f5045342350aa1a816bfb5dc5f6cab9045828
CHARLAT Jerome <jerome.charlat@orange.fr>
2018-04-09 15:48:25 +0200

    Merge branch 'master' of https://github.com/aramane/ProjetISITECH
    # Conflicts:
    #	view/v_headB.php

--- model/GenererCode.php
+++ model/GenererCode.php
@@ -4,16 +4,8 @@ class GenererCode
 {
 	function Code($length = 7) 
 	{
-		$date = date('Y-m');
-	    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	    $charactersLength = strlen($characters);
-	    $code = 'LOC-'.$date.'-';
-	    
-	    for ($i = 0; $i < $length; $i++)
-	    {
-	        $code .= $characters[rand(0, $charactersLength - 1)];
-	    }
-	    return $code;
+		
+            
 	}
 }
 
